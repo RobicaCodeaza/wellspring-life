a9864296d915d2d8f4cb595dd252da61f733c8b7
RobicaCodeaza <stoicarobert38@yahoo.com>
2025-04-20 17:11:26 +0300

    Deploy/Env setup

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://zfbhagjertdeaagzpuyz.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmYmhhZ2plcnRkZWFhZ3pwdXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxNTA2NjgsImV4cCI6MjA2MDcyNjY2OH0._OCXJNJrBhh_R1gxPQ8-hDR43cmWVQP2IMienfy_0LM';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
